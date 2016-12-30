aafb32024c052d2fa890da2fbe92eb26bbac54ff
Carl Seelye <cseelye@gmail.com>
2016-12-30 11:50:17 -0800

    Update setup.py with new name

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup
 import os
 
 setup(
-    name = "twowaymap",
+    name = "onetoonemap",
     version = "1.0.0",
     author = "Carl Seelye",
     author_email = "cseelye@gmail.com",
-    description = "Two-way mapping collection type",
+    description = "1:1 mapping collection type",
     license = "MIT",
-    keywords = "two-way map dict",
-    packages = ["twowaymap"],
-    url = "https://github.com/cseelye/twowaymap",
+    keywords = "1:1 map dict",
+    packages = ["onetoonemap"],
+    url = "https://github.com/cseelye/onetoonemap",
     long_description = open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
     install_requires = [
     ]
